7ca93efb7030a57ba3e13b86297a5e80113a28e4
exportroblox <clankergpt@gmail.com>
2026-03-27 10:31:09 +0000

    Update frames.js

--- api/frames.js
+++ api/frames.js
@@ -10,9 +10,9 @@ try {
   ffmpegPath = null;
 }
 
-const MAX_RES = 256;
-const MAX_FPS = 10;
-const MAX_DURATION = 15; // seconds
+const MAX_RES = 1000;
+const MAX_FPS = 30;
+const MAX_DURATION = 30; // seconds
 const DEFAULT_DELAY = 100; // ms
 
 // In-memory cache (same instance reuse)
